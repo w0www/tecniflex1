358235465e2e71a6e19f9caeaa0dca1496754894
Patricio Arluciaga <patricio.arluciaga@gmail.com>
2013-02-28 22:51:10 -0300

    Correcciones a informe de tiempos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,13 @@ class UsersController < ApplicationController
     @clies = Cliente.all
     @usuarios = User.all
     if params[:user].blank?
-	    @todas = []
-	  else
+	    @todas = User.paginate(:conditions => ["name = ?", "jeim"],:page => params[:page], :per_page => 35)
+	  elsif params[:startdate] && params[:enddate]
+	  	from_date = Date.strptime(params[:startdate],"%d/%m/%Y")
+	  	to_date = Date.strptime(params[:enddate],"%d/%m/%Y")
 	    @usuario = User.find(params[:user])
-      @todas = @usuario.intervencions
+      @todas = @usuario.intervencions.paginate(:conditions => ["created_at >= ? and created_at <= ?",from_date,to_date],:page => params[:page], :per_page => 35)
+
 		end
     hobo_ajax_response if request.xhr?
   end
