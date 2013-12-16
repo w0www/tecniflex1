1981a73900b76f49ddbf626247f1e3b05de0a672
Imanol Alvarez <imanol@alvarezperez.net>
2013-12-16 01:21:46 +0100

    reparar migraciones

--- db/migrate/20131002112850_hobo_migration_63.rb
+++ db/migrate/20131002112850_hobo_migration_63.rb
@@ -1,6 +1,8 @@
 class HoboMigration63 < ActiveRecord::Migration
   def self.up
+    add_column :existencias, :unidad_id, :integer
     remove_index :existencias, :name => :index_stocks_on_bodega_id rescue ActiveRecord::StatementInvalid
+    add_index :existencias, [:unidad_id]
 
     remove_column :movimientos, :separacion_id
     remove_index :movimientos, :name => :index_movimientos_on_separacion_id rescue ActiveRecord::StatementInvalid
@@ -9,11 +11,13 @@ class HoboMigration63 < ActiveRecord::Migration
   end
 
   def self.down
+    remove_column :existencias, :unidad_id
     add_index :existencias, [:bodega_id], :name => 'index_stocks_on_bodega_id'
 
     add_column :movimientos, :separacion_id, :integer
     add_index :movimientos, [:separacion_id]
 
+    remove_index :existencias, :name => :index_existencias_on_unidad_id rescue ActiveRecord::StatementInvalid
     remove_index :tareas, :name => :index_tareas_on_asignada_a rescue ActiveRecord::StatementInvalid
   end
 end
