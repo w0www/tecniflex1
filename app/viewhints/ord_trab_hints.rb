15a70d64156655053e463f048965f6d727da17ff
Patricio <patricio@UbuntuRailsTest.(none)>
2014-01-15 23:57:54 -0300

    Correcciones estéticas e incorporacion de logo Tecniflex

--- app/viewhints/ord_trab_hints.rb
+++ app/viewhints/ord_trab_hints.rb
@@ -1,6 +1,6 @@
 class OrdTrabHints < Hobo::ViewHints
 
-  model_name "O.T."
+  model_name "Ordenes"
   field_names :mtz => "Matriceria", :mtje => "Montaje", :film => "Filmacion", :vb => "Visto Bueno", :tipofotop => "Categoria"
   # field_help :field1 => "Enter what you want in this field"
   # children :primary_collection1, :aside_collection1, :aside_collection2
--- app/viewhints/user_hints.rb
+++ app/viewhints/user_hints.rb
@@ -1,6 +1,6 @@
 class UserHints < Hobo::ViewHints
 
-  # model_name "My Model"
+  model_name "Usuarios"
   field_names :area_mtz => "Matriceria", :area_mtje => "Montaje", :area_film => "Filmacion"
   inline_booleans true
   # field_help :field1 => "Enter what you want in this field"
