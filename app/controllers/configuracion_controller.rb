d9636013cb9ec640e10d6d6c7654ddf8b067cae6
Imanol Alvarez <imanol@alvarezperez.net>
2015-06-26 01:14:44 +0200

    fix

--- app/controllers/configuracion_controller.rb
+++ app/controllers/configuracion_controller.rb
@@ -5,10 +5,10 @@
   def index
     if params[:export_to_xml]
       confi = Configuration.find_by_key("export_to_xml")
-      confi.update_attribute(:value, "true") if confi
+      confi.update_attribute(:value, true) if confi
     elsif params[:not_export_to_xml]
       confi = Configuration.find_by_key("export_to_xml")
-      confi.update_attribute(:value, "false") if confi
+      confi.update_attribute(:value, false) if confi
     end
   end
 end
