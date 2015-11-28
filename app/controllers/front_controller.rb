d4a609c7c06585c57e33eae78022eeb48f0a17f4
Imanol Alvarez <imanol@alvarezperez.net>
2015-11-29 00:18:59 +0100

    actualizar will_paginate en polimeros

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -44,6 +44,26 @@ class FrontController < ApplicationController
     id_facturacion = Proceso.find_by_nombre("facturacion").id
     @tareas = Tarea.find(:all, :conditions => ["proceso_id IN (?) AND state IN (?)", [id_facturacion,id_polimero,id_revisionmm],["habilitada","iniciada", "detenida","enviada","recibida", "en_revision"]], :include => [:proceso => :grupoproc], :order => "grupoprocs.position")
 
+    # Calcular las paginas totales
+    if @tareas.count <= 20
+      paginas_totales = 1
+    elsif @tareas.count > 20
+      paginas_totales = (@tareas.count / 20) + 1      
+    end
+    # Entramos en /front/polimeros
+    if !params[:page]
+      params[:page] = 1
+      @pagina_siguiente = paginas_totales == 1 ? 1 : 2
+    elsif params[:page]
+      # Si recibimos params[:id]
+      if paginas_totales > 1
+        @pagina_siguiente = params[:page].to_i + 1
+        if params[:page].to_i == paginas_totales
+          @pagina_siguiente = 1
+        end
+      end
+    end
+    @tareas = @tareas.paginate(:page => params[:page], :per_page => 20)
   end
   
   def eliminar
