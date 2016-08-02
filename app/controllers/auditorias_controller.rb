fb7b1e5fccf36d01a5e1f36cf0d8dcfe1772042e
w0www <imanol@alvarezperez.net>
2016-08-02 16:15:35 +0200

    generar csb

--- app/controllers/auditorias_controller.rb
+++ app/controllers/auditorias_controller.rb
@@ -11,7 +11,51 @@ class AuditoriasController < ApplicationController
         final = Date.strptime(params[:enddate], '%d/%m/%Y').to_time.end_of_day if params[:enddate] && !params[:enddate].blank?
         @auditorias = Auditoria.apply_scopes(:created_between => [inicial, final]).paginate(:page => params[:page], :per_page => 20)
       end
+      wants.csv do
+        csv_string = CSV.generate(:col_sep => ";") do |csv|
+          ##################
+          @fechini = params[:startdate] && params[:startdate].blank? ? "" : Date.strptime(params[:startdate], "%d/%m/%Y")
+          @fenal = params[:enddate] && params[:enddate].blank? ? "" : Date.strptime(params[:enddate], "%d/%m/%Y")
+          
+          if @fechini != "" && @fenal.blank?
+            @auditorias = Auditoria.all(:conditions => ["created_at >= ?", @fechini.to_datetime.in_time_zone(Time.zone)])            
+          elsif @fechini.blank? && @fenal != ""
+            @auditorias = Auditoria.all(:conditions => ["created_at <= ?", @fenal.to_datetime.in_time_zone(Time.zone)])
+          elsif @fechini != "" && @fenal != ""
+            @auditorias = Auditoria.all(:conditions => ["created_at >= ? and created_at <= ?",
+                          @fechini.to_datetime.in_time_zone(Time.zone), @fenal.to_datetime.in_time_zone(Time.zone)])
+          end
+
+          ##################
+          arre = ["TIPO", "FECHA", "USUARIO", "ORDEN", "DETALLES"]
+          csv << arre
+          ## data rows
+            @auditorias.each do |a|
+              # Tipo
+              @tipo = a.tipo ? a.tipo : ""
+              # Fecha
+              @fecha = a.fecha.strftime("%d/%m/%Y %H:%M:%S")
+              # Usuario
+              @usuario = a.user ? a.user : ""
+              # NRO OT
+              @orden = a.ord_trab ? a.ord_trab : ""
+              # Detalles
+              @detalles = a.detalles ? a.detalles : ""
+              arri = [@tipo, @fecha, @usuario, @orden, @detalles] 
+              csv << arri
+            end
+          				
+        # send it to da browsah
+        end
+        send_data(csv_string,
+                  :type => 'text/csv; charset=iso-8859-1; header=present',
+                  :disposition => "attachment", :filename => Time.now.strftime("Auditorias %d_%m_%Y") + ".csv")
+      end
     end
   end
 
 end
+
+
+
+
