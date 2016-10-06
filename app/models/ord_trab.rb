a4b4e685b1bf03982b7df472d5738575d8220eac
w0www <imanol@alvarezperez.net>
2016-10-06 11:04:53 +0200

    cambios septiembre

--- app/models/ord_trab.rb
+++ app/models/ord_trab.rb
@@ -352,7 +352,8 @@ class OrdTrab < ActiveRecord::Base
   validates_presence_of :encargado_id
   validates_presence_of :dispBandas, :espesor, :tipomat, :if => "self.mtz"
   validates_associated :separacions, :if => "(self.mtje || self.mtz || self.pol) && self.activa? ", :on => :habilitar
-  validate :limite_codigo_barras, :barcodes_iguales
+
+  validate :limite_codigo_barras, :barcodes_iguales, :pasosybandas
 
   def limite_codigo_barras
     if list_barcode
@@ -363,6 +364,20 @@ class OrdTrab < ActiveRecord::Base
   def barcodes_iguales
     errors.add(:barcodecopy, "tiene que ser igual que el barcode") if !barcode.blank? && barcode != barcodecopy
   end
+
+  def pasosybandas
+    if self.mtz || self.mtje
+      if self.nPasos < 1 && self.nBandas < 1
+        errors.add(:nPasos, "El número de pasos tiene que ser mayor que 0")
+        errors.add(:nBandas, "El número de bandas tiene que ser mayor que 0")
+      elsif self.nPasos < 1 && self.nBandas >= 1
+        errors.add(:nPasos, "El número de pasos y bandas tiene que ser mayor que 0")
+      elsif self.nPasos >=1 && self.nBandas < 1
+        errors.add(:nBandas, "El número de bandas tiene que ser mayor que 0")
+      end
+    end
+  end
+
 
   def before_create
 		if OrdTrab.all == []
@@ -435,9 +450,8 @@ class OrdTrab < ActiveRecord::Base
   end
 
   def after_create
-    Auditoria.create(
-      :tipo => "creación",:fecha => DateTime.now, :user_id => acting_user.id, :ord_trab_id => self.id, :detalles => "#{self.inspect}"
-    )
+    usuario = acting_user.id if acting_user
+    Auditoria.create(:tipo => "creación",:fecha => DateTime.now,:user_id => usuario, :ord_trab_id => self.id, :detalles => "#{self.inspect}" )
   end
 
   def before_destroy
@@ -510,8 +524,6 @@ class OrdTrab < ActiveRecord::Base
   end
 
   def before_save
-    self.nBandas = 1 unless self.nBandas?
-    self.nPasos = 1 unless self.nPasos?
     self.nCopias = 1 unless self.nCopias?
 
 		sarr = ["vb", "ptr", "mtz", "mtje"]
